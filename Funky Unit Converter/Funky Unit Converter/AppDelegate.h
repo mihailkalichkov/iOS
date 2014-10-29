b6b5e39073e72b0496894e40f69cb9167366bd8e
mihailkalichkov <mihailkalichkov@Mihails-MacBook-Pro.local>
2014-10-29 15:31:03 +0200

    start-up

---
+++ Funky Unit Converter/Funky Unit Converter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Funky Unit Converter
+//
+//  Created by Mihail Kalichkov on 10/29/14.
+//  Copyright (c) 2014 Mihail Kalichkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Funky Unit Converter/Funky Unit Converter/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Funky Unit Converter
+//
+//  Created by Mihail Kalichkov on 10/29/14.
+//  Copyright (c) 2014 Mihail Kalichkov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
