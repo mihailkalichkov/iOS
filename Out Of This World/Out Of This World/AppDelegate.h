29c58d327cd5bf26645407e3ff0130fd13513ba6
mihailkalichkov <mihailkalichkov@Mihails-MacBook-Pro.local>
2014-11-27 22:13:22 +0200

    TableView demo

---
+++ Out Of This World/Out Of This World/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Out Of This World
+//
+//  Created by Mihail Kalichkov on 11/27/14.
+//  Copyright (c) 2014 Mihail Kalichkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
