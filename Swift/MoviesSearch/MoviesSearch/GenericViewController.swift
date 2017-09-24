e58704fec592d247a293066c8b23519de27ef15c
Mihail Kalichkov <mihailkalichkov@Mihails-Mac-mini.local>
2017-09-24 19:13:31 +0300

    refactoring

--- Swift/MoviesSearch/MoviesSearch/GenericViewController.swift
+++ Swift/MoviesSearch/MoviesSearch/GenericViewController.swift
@@ -14,4 +14,14 @@ class GenericViewController: UIViewController {
         let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: storyboardID) as! GenericViewController
         return vc
     }
+    
+    public func presentAlertWithTitle(title: String, message : String, alertActionClosure : @escaping ((UIAlertAction) -> Void)){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: { (alertAction) in
+            alertActionClosure(alertAction)
+            alert.dismiss(animated: true, completion: nil)
+        })
+        alert.addAction(okAction)
+        self.present(alert, animated: true, completion: nil)
+    }
 }
--- Swift/MoviesSearch/MoviesSearch/SearchViewController.swift
+++ Swift/MoviesSearch/MoviesSearch/SearchViewController.swift
@@ -18,33 +18,36 @@ class SearchViewController: GenericViewController {
     let urlString = "https://api.themoviedb.org/3/search/movie"
     let apiKey = "7b64ae78d2898a533fc0b81d5b1b4411"
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    @IBAction func searchPressed(_ sender: UIButton) {
+        if titleTextField.text?.count == 0 {//basic validation for example purposes
+            self.presentAlertWithTitle(title: "Error", message: "Title must be longer than 0 characters", alertActionClosure: { (action) in
+                //handle alertAction here
+            })
+        }else{
+            self.createSearchCriteria()
+        }
     }
     
-    @IBAction func searchPressed(_ sender: UIButton) {
-        if titleTextField.text?.count == 0 {
-            let alert = UIAlertController(title: "Error", message: "Title must be longer than 0 characters", preferredStyle: .alert)
-            let okAction = UIAlertAction(title: "OK", style: .default, handler: { (alertAction) in
-                alert.dismiss(animated: true, completion: nil)
-            })
-            alert.addAction(okAction)
-            self.present(alert, animated: true, completion: nil)
-        }else{
-            let searchCriteria = SearchCriteria(name: titleTextField.text!, year: yearTextField.text!, genre: genreTextField.text!)
-            Alamofire.request(urlString, parameters: ["api_key": apiKey, "query": searchCriteria.name , "year": searchCriteria.year , "search_type": searchCriteria.genre]).responseJSON { (json) in
-                let moviesJSONObject = try? JSONSerialization.jsonObject(with: json.data!) as! [String: AnyObject]
-                let movieResults = moviesJSONObject!["results"] as! [AnyObject]
-                var moviesArray = [Movie]()
-                for movie in movieResults{
-                    moviesArray.append(Movie(dic: movie as! [String : AnyObject]))
-                }
-                let moviesListViewController = self.createViewController(storyboardID: "MoviesListViewController") as! MoviesListViewController
-                moviesListViewController.moviesArray = moviesArray
-                self.navigationController?.pushViewController(moviesListViewController, animated: true)
+    private func createSearchCriteria() -> Void{
+        let searchCriteria = SearchCriteria(name: titleTextField.text!, year: yearTextField.text!, genre: genreTextField.text!)
+        self.sendSearchCriteria(criteria: searchCriteria)
+    }
+    
+    private func sendSearchCriteria(criteria : SearchCriteria) -> Void{
+        Alamofire.request(urlString, parameters: ["api_key": apiKey, "query": criteria.name , "year": criteria.year , "search_type": criteria.genre]).responseJSON { (json) in
+            let moviesJSONObject = try? JSONSerialization.jsonObject(with: json.data!) as! [String: AnyObject]
+            let movieResults = moviesJSONObject!["results"] as! [AnyObject]
+            var moviesArray = [Movie]()
+            for movie in movieResults{
+                moviesArray.append(Movie(dic: movie as! [String : AnyObject]))
             }
+            self.pushMoviesListViewControllerWithMovies(movies: moviesArray)
         }
+    }
+    
+    private func pushMoviesListViewControllerWithMovies(movies : [Movie]){
+        let moviesListViewController = self.createViewController(storyboardID: "MoviesListViewController") as! MoviesListViewController
+        moviesListViewController.moviesArray = movies
+        self.navigationController?.pushViewController(moviesListViewController, animated: true)
     }
 }
